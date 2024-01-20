48eafaef98b36d0ec166ba5643950de1cd4fa416
Roxana Muntian <roxana@fiveem.com>
2024-01-20 22:44:39 +0200

    Added push and peek method to the stack. Added validations.

--- classes/stack.py
+++ classes/stack.py
@@ -5,6 +5,15 @@ class stack:
     def __len__(self):
         return len(self.__items)
 
+    def __push__(self, item):
+        return self.__items.append(item)
+
     def __pop__(self):
+        if len(self) == 0:
+            return Exception("Stack is empty. The pop operation cannot be perfromed.")
         return self.__items.pop(0)
-    
+
+    def __peek__(self):
+        if len(self) == 0:
+            return Exception('Stack is empty. The peek operation cannot be performed.')
+        return self.__items[-1]
